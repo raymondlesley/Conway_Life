5d8f1547fefa8a9d54588e4ef8041cd143d2c22a
Raymond Lesley <raymond.lesley@eon.com>
2018-07-11 14:03:09 +0100

    Refactor: use dict in place of 2D array (_MUCH_ quicker)

--- grid.py
+++ grid.py
@@ -16,41 +16,44 @@ class Grid:
     def __init__(self, rows, cols):
         self.__rows = rows
         self.__cols = cols
-        self.__grid = [[Cell(Cell.Void) for col in range(cols)] for row in range(rows)]
+        self.__grid = {}
 
     def __repr__(self):
         return "<Grid(%dx%d)>" % (self.__rows, self.__cols)
 
     def set_alive(self, row, col):
-        self.__grid[row][col] = Cell(Cell.Alive)
+        self.__grid[(row, col)] = Cell(Cell.Alive)
 
     def set_void(self, row, col):
-        self.__grid[row][col] = Cell(Cell.Void)
+        key = (row, col)
+        if key in self.__grid: del self.__grid[key]
 
     def is_alive(self, row, col):
-        return self.__grid[row][col].is_alive()
+        key = (row, col)
+        return key in self.__grid and self.__grid[key].is_alive()
 
     def is_void(self, row, col):
-        return self.__grid[row][col].is_void()
+        key = (row, col)
+        return not key in self.__grid or self.__grid[key].is_void()
 
-    def adjacent_live_cells(self, row, col):
+    def count_neighbours(self, row, col):
         num_live_cells = 0
         if row > 0:
-            if self.__grid[row - 1][col].is_alive(): num_live_cells += 1            # N
+            if self.is_alive(row - 1, col): num_live_cells += 1            # N
             if col > 0:
-                if self.__grid[row - 1][col - 1].is_alive(): num_live_cells += 1    # NW
+                if self.is_alive(row - 1, col - 1): num_live_cells += 1    # NW
             if col < (self.__cols - 1):
-                if self.__grid[row - 1][col + 1].is_alive(): num_live_cells += 1    # NE
+                if self.is_alive(row - 1, col + 1): num_live_cells += 1    # NE
         if row < (self.__rows - 1):
-            if self.__grid[row + 1][col].is_alive(): num_live_cells += 1            # S
+            if self.is_alive(row + 1, col): num_live_cells += 1            # S
             if col > 0:
-                if self.__grid[row + 1][col - 1].is_alive(): num_live_cells += 1    # SW
+                if self.is_alive(row + 1, col - 1): num_live_cells += 1    # SW
             if col < (self.__cols - 1):
-                if self.__grid[row + 1][col + 1].is_alive(): num_live_cells += 1    # SE
+                if self.is_alive(row + 1, col + 1): num_live_cells += 1    # SE
         if col > 0:
-            if self.__grid[row][col - 1].is_alive(): num_live_cells += 1            # W
+            if self.is_alive(row, col - 1): num_live_cells += 1            # W
         if col < (self.__cols - 1):
-            if self.__grid[row][col + 1].is_alive(): num_live_cells += 1            # E
+            if self.is_alive(row, col + 1): num_live_cells += 1            # E
         return num_live_cells
 
     def tick(self):
@@ -58,20 +61,20 @@ class Grid:
         new_grid = copy.deepcopy(self.__grid)
         for row in range(self.__rows):
             for col in range(self.__cols):
-                cell = self.__grid[row][col]
-                num_adjent_live_cells = self.adjacent_live_cells(row, col)
-                if cell.is_alive():
+                cell = self.__grid.get((row, col))
+                num_adjent_live_cells = self.count_neighbours(row, col)
+                if cell and cell.is_alive():
                     if num_adjent_live_cells < 2:   # rule 1: <2 ... cell dies
-                        new_grid[row][col] = Cell(Cell.Void)
+                        del new_grid[(row, col)]
                     elif num_adjent_live_cells > 3:   # rule 3: >3 ... cell dies
-                        new_grid[row][col] = Cell(Cell.Void)
+                        del new_grid[(row, col)]
                 elif num_adjent_live_cells == 3:   # rule 4: ==3 ... cell born
-                    new_grid[row][col] = Cell(Cell.Alive)
+                    new_grid[(row, col)] = Cell(Cell.Alive)
         self.__grid = new_grid
 
 
     def as_grid_string(self):
-        return '\n'.join([''.join(['#' if self.__grid[row][col].is_alive() else ' ' for row in range(self.__rows)]) for col in range(self.__cols)])
+        return '\n'.join([''.join(['#' if self.is_alive(row, col) else ' ' for row in range(self.__rows)]) for col in range(self.__cols)])
 
 ## ####################################################################### ##
 
@@ -90,26 +93,26 @@ class CellTests(unittest.TestCase):
     def test_create_grid(self):
         grid = self.create_test_scenario()
         self.assertTrue(grid)
-        self.assertTrue(grid.adjacent_live_cells(0, 0) == 1)
-        self.assertTrue(grid.adjacent_live_cells(0, 1) == 1)
-        self.assertTrue(grid.adjacent_live_cells(0, 2) == 1)
-        self.assertTrue(grid.adjacent_live_cells(0, 3) == 0)
-        self.assertTrue(grid.adjacent_live_cells(0, 4) == 0)
-        self.assertTrue(grid.adjacent_live_cells(1, 0) == 1)
-        self.assertTrue(grid.adjacent_live_cells(1, 2) == 3)
-        self.assertTrue(grid.adjacent_live_cells(1, 3) == 2)
-        self.assertTrue(grid.adjacent_live_cells(1, 4) == 1)
-        self.assertTrue(grid.adjacent_live_cells(2, 0) == 2)
-        self.assertTrue(grid.adjacent_live_cells(2, 1) == 4)
-        self.assertTrue(grid.adjacent_live_cells(2, 4) == 1)
-        self.assertTrue(grid.adjacent_live_cells(3, 0) == 1)
-        self.assertTrue(grid.adjacent_live_cells(3, 3) == 3)
-        self.assertTrue(grid.adjacent_live_cells(3, 4) == 1)
-        self.assertTrue(grid.adjacent_live_cells(4, 0) == 1)
-        self.assertTrue(grid.adjacent_live_cells(4, 1) == 2)
-        self.assertTrue(grid.adjacent_live_cells(4, 2) == 2)
-        self.assertTrue(grid.adjacent_live_cells(4, 3) == 1)
-        self.assertTrue(grid.adjacent_live_cells(4, 4) == 0)
+        self.assertTrue(grid.count_neighbours(0, 0) == 1)
+        self.assertTrue(grid.count_neighbours(0, 1) == 1)
+        self.assertTrue(grid.count_neighbours(0, 2) == 1)
+        self.assertTrue(grid.count_neighbours(0, 3) == 0)
+        self.assertTrue(grid.count_neighbours(0, 4) == 0)
+        self.assertTrue(grid.count_neighbours(1, 0) == 1)
+        self.assertTrue(grid.count_neighbours(1, 2) == 3)
+        self.assertTrue(grid.count_neighbours(1, 3) == 2)
+        self.assertTrue(grid.count_neighbours(1, 4) == 1)
+        self.assertTrue(grid.count_neighbours(2, 0) == 2)
+        self.assertTrue(grid.count_neighbours(2, 1) == 4)
+        self.assertTrue(grid.count_neighbours(2, 4) == 1)
+        self.assertTrue(grid.count_neighbours(3, 0) == 1)
+        self.assertTrue(grid.count_neighbours(3, 3) == 3)
+        self.assertTrue(grid.count_neighbours(3, 4) == 1)
+        self.assertTrue(grid.count_neighbours(4, 0) == 1)
+        self.assertTrue(grid.count_neighbours(4, 1) == 2)
+        self.assertTrue(grid.count_neighbours(4, 2) == 2)
+        self.assertTrue(grid.count_neighbours(4, 3) == 1)
+        self.assertTrue(grid.count_neighbours(4, 4) == 0)
         print(grid.as_grid_string())
         grid.tick()
         print(grid.as_grid_string())
